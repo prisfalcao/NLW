c4b8b6a51ebb29436a048cd83649ee9d8961e6c9
Priscila Falcão <falcao.priscila@hotmail.com>
2020-10-16 20:49:10 -0300

    NLW - Front

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,83 @@
+//create map
+const map = L.map('mapid').setView([-30.0345406,-51.1184903], 15);
+
+
+//create and add tileLayer
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+).addTo(map);
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+})
+
+let marker;
+
+
+//create and add marker
+map.on('click', (event) => {
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
+    //remove icon
+    marker && map.removeLayer(marker)
+
+    //add icon layer
+    marker = L.marker([lat, lng], {icon})
+    .addTo(map);
+})
+
+//photos upload
+function addPhotoField() {
+    const container = document.querySelector('#images')
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+
+    const input = newFieldContainer.children[0]
+    if(input.value == "") {
+        return 
+    }
+
+    input.value = ""
+
+    container.appendChild(newFieldContainer)
+}
+
+function deleteField(event) {
+    const span = event.currentTarget
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+    if(fieldsContainer.length < 2) {
+
+        span.parentNode.children[0].value=""
+        return
+    }
+
+    span.parentNode.remove()
+}
+
+//seleção botão sim ou não
+function toggleSelect(event) {
+
+    document.querySelectorAll('.button-select button')
+    .forEach((button) => {
+        button.classList.remove('active')
+    })
+
+    const button = event.currentTarget
+    button.classList.add('active')
+
+    const input = document.querySelector('[name="open-on-weekends"]')
+
+    input.value = button.dataset.value
+}
+
+
